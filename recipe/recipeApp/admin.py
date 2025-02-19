804d10290429532358e8cacdced6a7d133520538
annjo <annjomathew@gmail.com>
2025-02-19 18:07:53 +0530

    Favorite, Rating models added

--- recipe/recipeApp/admin.py
+++ recipe/recipeApp/admin.py
@@ -5,9 +5,14 @@ from .models import Recipe
 from .models import Ingredient
 from .models import RecipeIngredient
 from .models import Instruction
+from .models import Favorite
+from .models import Rating
+
 
 
 admin.site.register(Recipe)
 admin.site.register(Ingredient)
 admin.site.register(RecipeIngredient)
 admin.site.register(Instruction)
+admin.site.register(Favorite)
+admin.site.register(Rating)
--- recipe/recipeApp/models.py
+++ recipe/recipeApp/models.py
@@ -24,7 +24,7 @@ class RecipeIngredient(models.Model):
     measure = models.CharField(max_length=50, default="Enter measurement")
 
     def __str__(self):
-        return self.ingredient
+        return self.ingredient.ingredient_name
 
 
 class Instruction(models.Model):
@@ -42,3 +42,12 @@ class Favorite(models.Model):
 
     def __str__(self):
         return f"{self.user.username} favorited {self.recipe.recipe_name}"
+    
+class Rating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    rating = models.IntegerField()  # Example: 1-5 stars
+    comment = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.user.username} rated {self.recipe.recipe_name} {self.rating} stars"

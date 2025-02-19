3e20ad0a38715a7ee79059dad6e90a20457fa827
annjo <annjomathew@gmail.com>
2025-02-19 16:40:43 +0530

    Recipe model final

--- recipe/recipeApp/models.py
+++ recipe/recipeApp/models.py
@@ -3,11 +3,13 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Recipe(models.Model):
-    user= models.ForeignKey(User, on_delete=models.CASCADE)
-    recipe_name=models.CharField(max_length=225)
-    instructions=models.TextField()
-    image=models.ImageField(upload_to='recipe_images/',null=True,blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe_name = models.CharField(max_length=255)
+    image = models.ImageField(upload_to='recipe_images/', null=True, blank=True)
+    time_needed = models.CharField(max_length=50, default="Not specified")
+
 
     def __str__(self):
         return self.recipe_name
+
 

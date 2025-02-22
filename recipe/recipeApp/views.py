722aa24c7842e8b3b64ef9e87254ce88af3a81c7
annjo <annjomathew@gmail.com>
2025-02-22 15:04:59 +0530

    Added Timestamp

--- recipe/recipeApp/views.py
+++ recipe/recipeApp/views.py
@@ -4,3 +4,18 @@ from django.shortcuts import render, redirect
 def index(request):
     # Food.objects
     return render(request, 'recipeApp/index.html')
+
+def about(request):
+    return render(request, 'recipeApp/about.html')
+
+def blog_post(request):
+    return render(request, 'recipeApp/blog-post.html')
+
+def recipe_post(request):
+    return render(request, 'recipeApp/recipe-post.html')
+
+def contact(request):
+    return render(request, 'recipeApp/contact.html')
+
+def elements(request):
+    return render(request, 'recipeApp/elements.html')

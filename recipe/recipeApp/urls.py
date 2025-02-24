5fd345efca2be738416f8f6250337da84b2a0aae
annjo <annjomathew@gmail.com>
2025-02-24 14:02:38 +0530

    latest recipe display

--- recipe/recipeApp/urls.py
+++ recipe/recipeApp/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views  
 
 urlpatterns = [
-    path('', views.index, name='index'),  
+    path('', views.index, name='index'),
+    path('demo/', views.demo, name='demo'),
+    path('recipe/<str:recipe_name>/', views.recipe_detail, name='recipe_detail'),
+
 ]
--- recipe/recipeApp/views.py
+++ recipe/recipeApp/views.py
@@ -1,11 +1,15 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Recipe
 
 def index(request):
+    latest_recipes = Recipe.objects.order_by('-created_at')[:6]  # Fetch latest 6 recipes
+    return render(request, 'recipeApp/index.html', {'latest_recipes': latest_recipes})
+
+""" def index(request):
     recipes = Recipe.objects.all()  # Fetch all recipes
     return render(request, 'recipeApp/index.html')
-
-def about(request):
+     """
+""" def about(request):
     return render(request, 'recipeApp/about.html')
 
 def blog_post(request):
@@ -19,3 +23,25 @@ def contact(request):
 
 def elements(request):
     return render(request, 'recipeApp/elements.html')
+ """
+def demo(request):
+    recipes = Recipe.objects.all()  # Fetch all recipes
+    return render(request, 'recipeApp/demo.html',{'recipes':recipes})
+
+""" def latest_recipes(request):
+    latest_recipes = Recipe.objects.order_by('-created_at')[:6]  # Fetch latest 6 recipes
+    print(latest_recipes)  
+    return render(request, 'recipeApp/latest_recipes.html', {'latest_recipes': latest_recipes})
+
+def recipe_detail(request, recipe_name):
+    recipe = get_object_or_404(Recipe, recipe_name=recipe_name)
+    return render(request, 'recipeApp/recipe_detail.html', {'recipe': recipe})
+     """
+
+def recipe_detail(request, recipe_name):
+    recipe = get_object_or_404(Recipe, recipe_name=recipe_name)
+    latest_recipes = Recipe.objects.order_by('-created_at')[:6]  # Fetch latest 6 recipes
+    return render(request, 'recipeApp/index.html', {
+        'recipe': recipe,
+        'latest_recipes': latest_recipes
+    })

0cd78fa615bdb1eba662b7e38b354c4b189593f8
annjo <annjomathew@gmail.com>
2025-02-24 15:47:55 +0530

    Latest recipes picture added

--- recipe/recipeApp/urls.py
+++ recipe/recipeApp/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from . import views  
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', views.index, name='index'),
     path('recipe/<str:recipe_name>/', views.recipe_detail, name='recipe_detail'),
-
 ]
+
+if settings.DEBUG:  # Only serve media files in development
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
